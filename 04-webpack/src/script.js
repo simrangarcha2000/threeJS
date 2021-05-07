ba9efb14f8295959ab0ba29b4d03c9149ba45592
simrangarcha2000 <simranjeetkaur.garcha@dcmail.ca>
2021-05-07 17:35:13 -0400

    Commented Learnt Property for scale , position and rotation and grouped them and rotation group together

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -7,7 +7,7 @@ const scene = new THREE.Scene()
 
 //Mesh -> Geometry (shape) + material (How it looks)
 
-//Red Cube -> 1unit
+/*//Red Cube -> 1unit
 const geometry = new THREE.BoxGeometry(1,1,1)
 
 //Material 
@@ -23,7 +23,7 @@ const cube = new THREE.Mesh(geometry,material);
 cube.position.y = -0.6
 cube.position.z = 1*/
 //Use top property together as one
-cube.position.set(0.7,-0.6,1)
+//cube.position.set(0.7,-0.6,1)
 
 //Scale -> 
 /*cube.scale.x = 2
@@ -32,22 +32,59 @@ cube.scale.z = 0.5*/
 
 //Or -> Vector3
 
-cube.scale.set(2,0.5,0.5)
+/*cube.scale.set(2,0.5,0.5)
+
+//Changing order of rotation
+cube.rotation.reorder('YXZ')
 
 //Rotation -> Euler -> good value -> pi -> half rotation or use Math.PI
-cube.rotation.x = 3.14159
-//cube.rotation.y = Math.PI
+//cube.rotation.x = 3.14159
+//cube.rotation.y = Math.PI -> take a order
 cube.rotation.y = Math.PI * 0.25
-cube.rotation.z = Math.PI * 0.25
+cube.rotation.x = Math.PI * 0.25
 
 
 //So that position remains  1 
 //cube.position.normalize()
 
 //Add Mesh to Scene
-scene.add(cube)
+scene.add(cube)*/
 
-console.log(cube.position.length())
+//console.log(cube.position.length())
+
+//Creating a Group
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 1
+scene.add(group)
+
+//Cube01
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.LineBasicMaterial({color: "blue"})
+)
+
+group.add(cube1)
+
+//Cube02
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.LineBasicMaterial({color: "red"})
+)
+cube2.position.set(-1.3,0,0)
+
+group.add(cube2)
+
+
+//Cube03
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.LineBasicMaterial({color: "green"})
+)
+cube3.position.set(1.3,0,0)
+
+group.add(cube3)
+
 
 
 //Axes Helper 
@@ -77,12 +114,14 @@ camera.position.z = 3
 /*camera.position.x = 1
 camera.position.y = 1*/
 
+//Look at 
+//camera.lookAt(cube.position)
 
 
 //Always add everything to the scene
 scene.add(camera)
 
-console.log(cube.position.distanceTo(camera.position))
+//console.log(cube.position.distanceTo(camera.position))
 
 //Connect to HTML canvas
 const canvashtml = document.querySelector('.renderPro')
