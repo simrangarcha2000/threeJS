884a7549111853fb0de6ec6dc854bfb00de81aef
simrangarcha2000 <simranjeetkaur.garcha@dcmail.ca>
2021-05-07 16:27:49 -0400

    Moving towards 3D

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -1,1 +1,59 @@
 import './style.css'
+import * as THREE from 'three'
+
+//Creating a scene
+const scene = new THREE.Scene()
+
+//Mesh -> Geometry (shape) + material (How it looks)
+
+//Red Cube -> 1unit
+const geometry = new THREE.BoxGeometry(1,1,1)
+
+//Material 
+const material = new THREE.MeshBasicMaterial({
+    color: 'red'
+})
+
+//Creating Mesh
+const cube = new THREE.Mesh(geometry,material);
+
+//Add Mesh to Scene
+scene.add(cube)
+
+//Camera -> Point of view -> Not visible 
+//Two Parameter
+/**
+ *  1 - Field of View (Vertical) -> Degree
+ *  2 - Aspect Ration -> Width (Size of View Port ) width/height -> Width and height  
+ */
+
+//Sizes -> Aspect Ration
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height )
+
+//Change poition of Camera - Step Last
+camera.position.z = 3
+
+//Always add everything to the scene
+scene.add(camera)
+
+
+//Connect to HTML canvas
+const canvashtml = document.querySelector('.renderPro')
+
+//Render -> First create canvas in HTML
+const renderer = new THREE.WebGLRenderer({
+    //Connect HTML canvas
+    canvas: canvashtml
+})
+
+//Resize render
+
+renderer.setSize(sizes.width,sizes.height)
+
+//Scene and Camera 
+renderer.render(scene, camera)
