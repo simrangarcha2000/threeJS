084447804da41cc1e50a279f1b68b4b2d3d39bf0
simrangarcha2000 <simranjeetkaur.garcha@dcmail.ca>
2021-05-17 15:55:50 -0400

    Loaded materials , playing with transparency and maps , and with alpha maps

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -3,6 +3,24 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
+ * Textures
+ */
+
+const textureLoader = new THREE.TextureLoader()
+
+const doorColor = textureLoader.load('/textures/door/color.jpg')
+const alpha = textureLoader.load('/textures/door/alpha.jpg')
+const ambient = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const height = textureLoader.load('/textures/door/height.jpg')
+const normal = textureLoader.load('/textures/door/normal.jpg')
+const metal = textureLoader.load('/textures/door/metalness.jpg')
+const rough = textureLoader.load('/textures/door/roughness.jpg')
+const matCapTexture = textureLoader.load('/textures/matcaps/1.png')
+const gradientTexture = textureLoader.load('/textures/gradients/1.png')
+
+
+
+/**
  * Base
  */
 // Canvas
@@ -10,6 +28,55 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Object 
+ */
+
+const material = new THREE.MeshBasicMaterial({
+    //color: 'pink',
+    map : doorColor
+});
+
+
+//material.wireframe = true
+material.color.set('red')
+
+material.opacity = 0.5
+material.transparent = true
+material.alphaMap = alpha
+
+/**
+ *  //! Another Way 
+ * material.map = doorColor
+ * 
+ * For Color 
+ * material.color = new THREE.Color('green')
+ */
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereBufferGeometry(0.5,16,16),
+    material
+)
+
+sphere.position.x = -1.5
+
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(1,1),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusBufferGeometry(0.5, 0.2, 16,32),
+    material
+)
+
+torus.position.x = 1.5
+
+scene.add(plane,sphere, torus)
+
+
 
 /**
  * Sizes
@@ -65,6 +132,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //update objects
+    sphere.rotation.y = elapsedTime * 0.1
+    plane.rotation.y = elapsedTime * 0.1
+    torus.rotation.y = elapsedTime * 0.1
+
+    sphere.rotation.x = elapsedTime * 0.15
+    plane.rotation.x = elapsedTime * 0.15
+    torus.rotation.x = elapsedTime * 0.15
 
     // Update controls
     controls.update()
