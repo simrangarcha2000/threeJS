7514a727aaeccc1f20794b0bff6be0b4a43d2e5d
simrangarcha2000 <simranjeetkaur.garcha@dcmail.ca>
2021-05-24 21:45:21 -0400

    Basic

--- practice-01/src/script.js
+++ practice-01/src/script.js
@@ -1,28 +1,78 @@
 import './style.css'
 import * as THREE from 'three'
-import gsap from 'gsap'
+/*
+//Creating a scene
+const scene = new THREE.Scene()
 
-const  scene = new THREE.Scene();
-const mesh = new THREE.Mesh(
+
+
+//Cube01
+const cube1 = new THREE.Mesh(
     new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial()
+    new THREE.LineBasicMaterial({color: "blue"})
 )
 
-scene.add(mesh);
+scene.add(cube1)
 
+//Sizes -> Aspect Ration
 const sizes = {
-    width: 700,
-    height: 800
+    width: 800,
+    height: 600
 }
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.width/sizes.height,
-    1,
-    1000
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height )
+
+camera.position.z = 3
+
+//Always add everything to the scene
+scene.add(camera)
+
+//console.log(cube.position.distanceTo(camera.position))
+
+//Connect to HTML canvas
+const canvashtml = document.querySelector('.webgl')
+
+//Render -> First create canvas in HTML
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvashtml
+})
+
+
+renderer.setSize(sizes.width,sizes.height)
+
+//Scene and Camera 
+renderer.render(scene, camera)*/
+
+const scene = new THREE.Scene()
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.LineBasicMaterial({
+        color: "blue"
+    })
 )
+
+scene.add(cube);
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+
+camera.position.z = 3
 
 scene.add(camera)
 
-const render = new THREE.WebGL1Renderer()
+const canvas = document.querySelector('.webgl');
 
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+
+renderer.setSize(
+    sizes.width, sizes.height
+)
+
+renderer.render(scene, camera)
