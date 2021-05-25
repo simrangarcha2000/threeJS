f07da44805084e9a73276ccbd803551afadaa766
simrangarcha2000 <simranjeetkaur.garcha@dcmail.ca>
2021-05-24 21:09:32 -0400

    Practice -0

--- practice-01/src/script.js
+++ practice-01/src/script.js
@@ -1,140 +1,87 @@
 import './style.css'
 import * as THREE from 'three'
-import { Mesh } from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-//Creating a scene
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
 const scene = new THREE.Scene()
 
-//Mesh -> Geometry (shape) + material (How it looks)
-
-/*//Red Cube -> 1unit
-const geometry = new THREE.BoxGeometry(1,1,1)
-
-//Material 
-const material = new THREE.MeshBasicMaterial({
-    color: 'red'
-})
-
-//Creating Mesh
-const cube = new THREE.Mesh(geometry,material);
-
-//Making the mesh a 3D object
-/*cube.position.x = 0.7
-cube.position.y = -0.6
-cube.position.z = 1*/
-//Use top property together as one
-//cube.position.set(0.7,-0.6,1)
-
-//Scale -> 
-/*cube.scale.x = 2
-cube.scale.y = 0.5
-cube.scale.z = 0.5*/
-
-//Or -> Vector3
-
-/*cube.scale.set(2,0.5,0.5)
-
-//Changing order of rotation
-cube.rotation.reorder('YXZ')
-
-//Rotation -> Euler -> good value -> pi -> half rotation or use Math.PI
-//cube.rotation.x = 3.14159
-//cube.rotation.y = Math.PI -> take a order
-cube.rotation.y = Math.PI * 0.25
-cube.rotation.x = Math.PI * 0.25
-
-
-//So that position remains  1 
-//cube.position.normalize()
-
-//Add Mesh to Scene
-scene.add(cube)*/
-
-//console.log(cube.position.length())
-
-//Creating a Group
-const group = new THREE.Group()
-group.position.y = 1
-group.scale.y = 1
-scene.add(group)
-
-//Cube01
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.LineBasicMaterial({color: "blue"})
-)
-
-group.add(cube1)
-
-//Cube02
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.LineBasicMaterial({color: "red"})
-)
-cube2.position.set(-1.3,0,0)
-
-group.add(cube2)
-
-
-//Cube03
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.LineBasicMaterial({color: "green"})
-)
-cube3.position.set(1.3,0,0)
-
-group.add(cube3)
-
-
-
-//Axes Helper 
-
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
-
-
-
-//Camera -> Point of view -> Not visible 
-//Two Parameter
 /**
- *  1 - Field of View (Vertical) -> Degree
- *  2 - Aspect Ration -> Width (Size of View Port ) width/height -> Width and height  
+ * Object
  */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
-//Sizes -> Aspect Ration
+/**
+ * Sizes
+ */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height )
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-//Change poition of Camera - Step Last
-camera.position.z = 3
-/*camera.position.x = 1
-camera.position.y = 1*/
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-//Look at 
-//camera.lookAt(cube.position)
-
-
-//Always add everything to the scene
-scene.add(camera)
-
-//console.log(cube.position.distanceTo(camera.position))
-
-//Connect to HTML canvas
-const canvashtml = document.querySelector('.renderPro')
-
-//Render -> First create canvas in HTML
-const renderer = new THREE.WebGLRenderer({
-    //Connect HTML canvas
-    canvas: canvashtml
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-//Resize render
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
+scene.add(camera)
 
-renderer.setSize(sizes.width,sizes.height)
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
-//Scene and Camera 
-renderer.render(scene, camera)
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
