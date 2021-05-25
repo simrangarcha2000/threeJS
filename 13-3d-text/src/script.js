c11ffb0d0b3d210d5a2f975895f6de5b2bd08d8b
simrangarcha2000 <simranjeetkaur.garcha@dcmail.ca>
2021-05-25 13:28:43 -0400

    Donut Distributed

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -87,7 +87,21 @@ fontLoader.load(
                 matcap : matcap1
             })
             const donut = new THREE.Mesh(donutGeometry,donutMaterial)
+
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+
+            donut.rotation.x = Math.random()  * Math.PI
+            donut.rotation.y = Math.random()  * Math.PI
+
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
+
             scene.add(donut)
+
+
         }
     }
 )
