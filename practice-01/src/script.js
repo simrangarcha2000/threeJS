2d15c985359c453d5ef295c5760cbd5405bd1bbc
simrangarcha2000 <simranjeetkaur.garcha@dcmail.ca>
2021-05-24 22:13:37 -0400

    Chapter 04 done

--- practice-01/src/script.js
+++ practice-01/src/script.js
@@ -1,47 +1,8 @@
 import './style.css'
 import * as THREE from 'three'
-/*
-//Creating a scene
-const scene = new THREE.Scene()
+import * as dat from 'dat.gui'
 
-
-
-//Cube01
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.LineBasicMaterial({color: "blue"})
-)
-
-scene.add(cube1)
-
-//Sizes -> Aspect Ration
-const sizes = {
-    width: 800,
-    height: 600
-}
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height )
-
-camera.position.z = 3
-
-//Always add everything to the scene
-scene.add(camera)
-
-//console.log(cube.position.distanceTo(camera.position))
-
-//Connect to HTML canvas
-const canvashtml = document.querySelector('.webgl')
-
-//Render -> First create canvas in HTML
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvashtml
-})
-
-
-renderer.setSize(sizes.width,sizes.height)
-
-//Scene and Camera 
-renderer.render(scene, camera)*/
+const gui = new dat.GUI({ closed: true, width: 400})
 
 const scene = new THREE.Scene()
 
@@ -52,7 +13,20 @@ const cube = new THREE.Mesh(
     })
 )
 
+cube.position.set(1, 0.5,0.8)
+cube.scale.set(1, 0.5,0.8)
+cube.rotation.reorder('YXZ')
+cube.rotation.x = Math.PI
+cube.rotation.x = Math.PI * 0.25
+cube.position.normalize()
+
 scene.add(cube);
+
+//Axes Helper 
+
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
 
 const sizes = {
     width: 800,
